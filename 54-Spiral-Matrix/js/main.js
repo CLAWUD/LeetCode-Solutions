dfe3784557a88f764c0b0473813a823243b2c05c
clawud@proton.me <clawud@proton.me>
2024-08-31 01:20:19 +0530

    optimal

--- 54-Spiral-Matrix/js/main.js
+++ 54-Spiral-Matrix/js/main.js
@@ -1,1 +1,29 @@
-console.log('Hello, world! ~ JS')
+function spirmatrixlOrder(matrix) {
+    let a=[];
+    let n=matrix.length;
+    let m=matrix[0].length;
+    
+    let top=0;
+    let bottom=n-1;
+    let left=0;
+    let right=m-1;
+    while(top<=bottom && left<=right){
+        for(let i=left;i<=right;i++){
+            a.push(matrix[top][i]);
+        }
+        top++;
+        for(let i=top;i<=bottom;i++){
+            a.push(matrix[i][right])
+        }
+        right--;
+        for(let i=right;i>=left;i--){
+            a.push(matrix[bottom][i])
+        }
+        bottom--;
+        for(let i=bottom;i>=top;i--){
+            a.push(matrix[i][left])
+        }
+        left++;
+    }
+    return a;
+}
