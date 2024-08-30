d7bd5ea0e2480a7f7a20d9f72bdd962d59cec28b
clawud@proton.me <clawud@proton.me>
2024-08-31 01:21:35 +0530

    optimal O(n)

--- 53-Maximum-Subarray/js/main.js
+++ 53-Maximum-Subarray/js/main.js
@@ -1,1 +1,14 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxi=nums[0];
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        sum += nums[i];
+        if(sum > maxi) maxi=sum;
+        if(sum < 0) sum=0;
+    }
+    return maxi;
+};
