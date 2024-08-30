5d27130555fb4a9c107b25b2ecb4dca205c929c5
clawud@proton.me <clawud@proton.me>
2024-08-31 01:14:06 +0530

    optimal

--- 118-Pascal-Triangle/js/main.js
+++ 118-Pascal-Triangle/js/main.js
@@ -1,1 +1,27 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+function nCr(n, r) {
+    let res = 1;
+  
+    // calculating nCr:
+    for (let i = 0; i < r; i++) {
+      res = res * (n - i);
+      res = res / (i + 1);
+    }
+    return parseInt(res);
+}
+var generate = function(numRows) {
+    const ans = [];
+
+// Store the entire Pascal's triangle:
+for (let row = 1; row <= numRows; row++) {
+  const tempLst = []; // temporary list
+  for (let col = 1; col <= row; col++) {
+    tempLst.push(nCr(row - 1, col - 1));
+  }
+  ans.push(tempLst);
+}
+return ans;
+};
