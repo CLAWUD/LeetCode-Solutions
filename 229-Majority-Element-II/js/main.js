386318b9b6039f8b1a905a90115fe88421bbeeff
clawud@proton.me <clawud@proton.me>
2024-08-31 01:11:20 +0530

    optimal

--- 229-Majority-Element-II/js/main.js
+++ 229-Majority-Element-II/js/main.js
@@ -1,1 +1,28 @@
-console.log('Hello, world! ~ JS')
+function majority(nums){
+    let n = nums.length;
+    let cnt = 0; 
+    let el; 
+
+    for (let i = 0; i < n; i++) {
+        if (cnt === 0) {
+            cnt = 1;
+            el = nums[i];
+        } else if (el === nums[i]) {
+            cnt++;
+        } else {
+            cnt--;
+        }
+    }
+
+    let cnt1 = 0;
+    for (let i = 0; i < n; i++) {
+        if (nums[i] === el) {
+            cnt1++;
+        }
+    }
+
+    if (cnt1 > Math.floor(n / 3)) {
+        return el;
+    }
+    return -1;
+}
