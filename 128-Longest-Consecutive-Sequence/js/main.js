9c2d967898846af9fa58d21a99e095cb1e444632
clawud@proton.me <clawud@proton.me>
2024-08-31 01:17:09 +0530

    optimal O(n)

--- 128-Longest-Consecutive-Sequence/js/main.js
+++ 128-Longest-Consecutive-Sequence/js/main.js
@@ -1,1 +1,22 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    if(nums.length==0) return 0;
+    nums.sort((a, b) => a - b);
+    let count=1;
+    let longest=1;
+    let lastMin=nums[0];
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]-1==lastMin){
+            count++;
+            lastMin=nums[i];
+        } else if(nums[i] != lastMin){
+            count=1;
+            lastMin=nums[i];
+        }
+        longest=Math.max(longest,count);
+    }
+    return longest;
+};
