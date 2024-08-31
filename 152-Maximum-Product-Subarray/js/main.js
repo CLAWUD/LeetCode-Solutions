5028e433a7111e88347480dfa5b1aab42eb13b30
clawud@proton.me <clawud@proton.me>
2024-08-31 13:57:00 +0530

    optimal O(n)

--- 152-Maximum-Product-Subarray/js/main.js
+++ 152-Maximum-Product-Subarray/js/main.js
@@ -1,1 +1,14 @@
-console.log('Hello, world! ~ JS')
+var maxProduct = function(nums) {
+  let prefix=1;
+  let suffix=1;
+  let maxP=nums[0];
+  for(let i=0;i<nums.length;i++){
+    if(prefix == 0) prefix=1;
+    if(suffix == 0) suffix=1;
+    prefix = prefix * nums[i];
+    suffix = suffix * nums[nums.length-i-1];
+    maxP=Math.max(maxP,Math.max(prefix,suffix));
+  }  
+  return maxP;
+};
+
