46a36afd6803c644b1b8ddd6dadf911e3747ab6b
clawud@proton.me <clawud@proton.me>
2024-09-01 23:19:25 +0530

    153-Find-Min-In-Rotated-Array O(logn)

--- 153-Find-Minimum-In-Rotated-Array/js/main.js
+++ 153-Find-Minimum-In-Rotated-Array/js/main.js
@@ -1,1 +1,20 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let low = 0;
+    let high = nums.length - 1;
+    let min = nums[0];
+    while (low <= high){
+        let mid = Math.floor((low + high) / 2);
+        if (nums[low] <= nums[mid]){
+            if (min >= nums[low]) min=nums[low];
+            low = mid + 1;
+        } else {
+            min = Math.min(min,nums[mid]);
+            high = mid - 1;
+        }
+    }
+    return min;
+};
