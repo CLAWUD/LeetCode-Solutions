a3bcf2963c2c526a4767b6d7dab7c3e4ab685593
clawud@proton.me <clawud@proton.me>
2024-09-10 22:36:57 +0530

    58-Length Of Last Word

--- 58-Length-Of-Last-Word/js/main.js
+++ 58-Length-Of-Last-Word/js/main.js
@@ -1,1 +1,14 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let i = s.length - 1;
+    let length = 0;
+    while (i >= 0 && s[i] == ' ') i--;
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+    return length;
+};
