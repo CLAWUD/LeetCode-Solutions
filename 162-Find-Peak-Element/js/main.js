cb7d7142cf18ff349b6efa6b367e6b602503a044
clawud@proton.me <clawud@proton.me>
2024-09-10 22:26:43 +0530

    162-Find Peak Element O(logn)

--- 162-Find-Peak-Element/js/main.js
+++ 162-Find-Peak-Element/js/main.js
@@ -1,1 +1,18 @@
-console.log('Hello, world! ~ JS')
+var findPeakElement = function(nums) {
+    let low = 0;
+    let high = nums.length - 1;
+
+    if (nums.length === 1) return 0;
+
+    while (low < high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] > nums[mid + 1]) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return low;
+};
