0f454a02ba0f40f66454b9ad4b7d077dea64c08e
clawud@proton.me <clawud@proton.me>
2024-09-13 00:06:25 +0530

    1482 Minimum Number of days to make M bouquets O(N * Log(max(bloomDay)))

--- 1482-Minimum-Number-Of-Days-To-Make-M-Bouquets/js/main.js
+++ 1482-Minimum-Number-Of-Days-To-Make-M-Bouquets/js/main.js
@@ -1,1 +1,37 @@
-console.log('Hello, world! ~ JS')
+function possible(bloomDay, day, m, k) {
+    let n = bloomDay.length; 
+    let cnt = 0;
+    let noOfB = 0;
+    for (let i = 0; i < n; i++) {
+        if (bloomDay[i] <= day) {
+            cnt++;
+        } else {
+            noOfB += Math.floor(cnt / k);
+            cnt = 0;
+        }
+    }
+    noOfB += Math.floor(cnt / k);
+    return noOfB >= m;
+}
+
+function minDays(bloomDay, m, k) {
+    let val = m * k;
+    let n = bloomDay.length;
+    if (val > n) return -1;
+    let mini = Infinity, maxi = -Infinity;
+    for (let i = 0; i < n; i++) {
+        mini = Math.min(mini, bloomDay[i]);
+        maxi = Math.max(maxi, bloomDay[i]);
+    }
+
+    let low = mini, high = maxi;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (possible(bloomDay, mid, m, k)) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
--- 2264-Largest-3-Same-/js/main.js
+++ 2264-Largest-3-Same-/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello, world! ~ JS')
+
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestGoodInteger = function(num) {
+    let str =  111 || 222 || 333 || 444 || 555 || 666 || 777 || 888 || 999;
+    if (nums.includes(str)) return str;
+};
+
+console.log(largestGoodInteger(133356));
