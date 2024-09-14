05016ff151ee70cd13a9852a5843b323446e6f40
clawud@proton.me <clawud@proton.me>
2024-09-15 01:45:07 +0530

    1283 Find the smallest divisor given a threshold O(N * log(max(nums)))

--- 1283-Find-The-Smallest-Divisor-Given-A-Threshold/js/main.js
+++ 1283-Find-The-Smallest-Divisor-Given-A-Threshold/js/main.js
@@ -1,1 +1,26 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+var smallestDivisor = function(nums, threshold) {
+    let low=1;
+    let high=Math.max(...nums);
+    while(low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if(sumbyD(nums,mid) <= threshold) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return low;
+};
+
+function sumbyD(nums, div) {
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        sum += Math.ceil(nums[i] / div);
+    }
+    return sum;
+}
