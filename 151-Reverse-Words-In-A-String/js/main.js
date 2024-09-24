febf1ba58fb657df3a4915a2654fa1bd4336e8e2
clawud@proton.me <clawud@proton.me>
2024-09-24 20:11:58 +0530

    151-Reverse words in a string O(n)

--- 151-Reverse-Words-In-A-String/js/main.js
+++ 151-Reverse-Words-In-A-String/js/main.js
@@ -1,1 +1,25 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let word = '';
+    let i = s.length - 1;
+    let result = '';
+    while (i >= 0) {
+        while (i >= 0 && s[i] === ' ') i--;
+        while (i >= 0 && s[i] !== ' ') {
+            word = s[i] + word;
+            i--;
+        }
+
+        if (word !== '') {
+            if (result !== '') {
+                result += ' ';
+            }
+            result += word;
+        }
+        word = '';
+    }
+    return result;
+};
