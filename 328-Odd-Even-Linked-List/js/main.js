8cddcfb17c3a1196c52dfdc65866c6ee99a75ad6
clawud@proton.me <clawud@proton.me>
2024-10-01 22:51:29 +0530

    328 Odd even Linked List O(n)

--- 328-Odd-Even-Linked-List/js/main.js
+++ 328-Odd-Even-Linked-List/js/main.js
@@ -1,1 +1,18 @@
-console.log('Hello, world! ~ JS')
+var oddEvenList = function(head) {
+    if (head === null || head.next === null) return head;
+
+    let odd = head;
+    let even = head.next;
+    let evenHead = even;
+
+    while (even !== null && even.next !== null) {
+        odd.next = odd.next.next;
+        even.next = even.next.next;
+
+        odd = odd.next;
+        even = even.next;
+    }
+
+    odd.next = evenHead;
+    return head;
+};
