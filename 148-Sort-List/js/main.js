33ce1fc546beb23e2078e7277c077e52efc3f651
clawud@proton.me <clawud@proton.me>
2024-10-04 22:51:56 +0530

    148-Sort List O(nlogn)

--- 148-Sort-List/js/main.js
+++ 148-Sort-List/js/main.js
@@ -1,1 +1,34 @@
-console.log('Hello, world! ~ JS')
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    if (!head) return null;
+
+    let arr = [];
+    let current = head;
+
+    while (current) {
+        arr.push(current.val);
+        current = current.next;
+    }
+
+    arr.sort((a, b) => a - b);
+
+    let newll = new ListNode(0);
+    current = newll;
+
+    for (let val of arr) {
+        current.next = new ListNode(val);
+        current = current.next;
+    }
+
+    return newll.next;
+};
