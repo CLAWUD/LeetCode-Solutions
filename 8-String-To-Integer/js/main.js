4274bd89c1e7a3544ab7b0cdab961672dcc67fd7
clawud@proton.me <clawud@proton.me>
2025-01-22 22:24:43 +0530

    8. myAtoi O(n)

--- 8-String-To-Integer/js/main.js
+++ 8-String-To-Integer/js/main.js
@@ -1,1 +1,37 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function(s) {
+    if (s.length === 0) return 0;
+
+   let i = 0;
+
+   while (i < s.length && s[i] === ' ') {
+       i++;
+   }
+
+   if (i >= s.length) return 0;
+
+   let sign = 1;
+   if (s[i] === '-' || s[i] === '+') {
+       sign = s[i] === '-' ? -1 : 1;
+       i++;
+   }
+
+   let ans = 0;
+   const MAX = Math.pow(2, 31) - 1; 
+   const MIN = -Math.pow(2, 31);  
+
+   
+   while (i < s.length && s[i] >= '0' && s[i] <= '9') {
+       ans = ans * 10 + (s[i] - '0');
+
+       if (sign === -1 && -ans < MIN) return MIN;
+       if (sign === 1 && ans > MAX) return MAX;
+
+       i++;
+   }
+
+   return sign * ans;
+};
