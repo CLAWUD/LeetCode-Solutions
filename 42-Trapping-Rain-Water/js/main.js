dce31d863a31190ee190f8e7f5485aac85ab743d
clawud@proton.me <clawud@proton.me>
2025-03-07 22:59:28 +0530

    42 Trapping Rain water O(N)

--- 42-Trapping-Rain-Water/js/main.js
+++ 42-Trapping-Rain-Water/js/main.js
@@ -1,1 +1,31 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+var trap = function(height) {
+    let lMax = rMax = total =0;
+    let l = 0;
+    let r = height.length - 1;
+    while (l < r) {
+        if(height[l] < height[r]) {
+            if(lMax > height[l]){
+                total += lMax - height[l]
+            } else {
+                lMax = height[l]
+            }
+            l++
+        } else {
+            if(rMax > height[r]){
+                total += rMax - height[r]
+            } else {
+                rMax = height[r]
+            }
+            r--;
+        }
+    }
+    return total;
+};
+
+let height = [0,1,0,2,1,0,1,3,2,1,2,1]
+console.log(trap(height))
--- Infix-To-Postfix/cpp/main.cpp
+++ Infix-To-Postfix/cpp/main.cpp
@@ -2,4 +2,71 @@
 
 using namespace std;
 
-int main(){cout<<"Hello, world! ~ C++"<<endl;return 0;}
+int precedence(char symbol){
+    switch (symbol)
+    {
+    case '^':
+        return 3;
+        break;
+    
+    case '/':
+        return 2;
+        break;
+    
+    case '*':
+        return 2;
+        break;
+
+    case '+':
+        return 1;
+        break;
+    
+    case '-':
+        return 1;
+        break;
+    
+    default:
+        return 0;
+        break;
+    }
+}
+
+string infixToPostfix(string s){
+    stack<char> st;
+    int i = 0 ;
+    string ans = "";
+    int n = s.size();
+    while (i < n){
+        if(s[i] >= 'A' && s[i] <= 'Z' ||
+           s[i] >= 'a' && s[i] <= 'z') {
+            ans.push_back(s[i]);
+        } else {
+            if(st.empty()){
+                st.push(s[i]);
+            } else {
+            if(precedence(s[i]) >= precedence(st.top())){
+                st.push(s[i]);
+            } else {
+                while(precedence(s[i]) >= precedence(st.top())){
+                    ans.push_back(st.top());
+                    st.pop();
+                }
+            }
+        }
+        while(st.empty()){
+            ans.push_back(st.top());
+        }
+        }
+    }
+    return ans;
+}
+
+
+int main() {
+    string Exp = "A+B*C";
+    string Answer = infixToPostfix(Exp);
+    cout<<Answer;
+}
+
+
+
