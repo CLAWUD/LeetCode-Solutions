30150bbe23679510d24c2507b991f2bbcac64c18
clawud@proton.me <clawud@proton.me>
2025-03-10 00:07:38 +0530

    735-Asteriod Collision O(N)

--- 735-Asteriod-Collision/js/main.js
+++ 735-Asteriod-Collision/js/main.js
@@ -1,1 +1,40 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number[]} asteroids
+ * @return {number[]}
+ */
+var asteroidCollision = function(asteroids) {
+    let stack = [];
+
+    for (let i = 0; i < asteroids.length; i++) {
+        let asteroid = asteroids[i];
+        let destroyed = false;
+
+        while (
+            stack.length > 0 && 
+            stack[stack.length - 1] > 0 && 
+            asteroid < 0
+        ) {
+            let last = stack[stack.length - 1];
+
+            if (last + asteroid === 0) { 
+                stack.pop();
+                destroyed = true;
+                break;
+            } else if (last + asteroid < 0) { 
+                stack.pop();
+            } else {
+                destroyed = true;
+                break;
+            }
+        }
+
+        if (!destroyed) {
+            stack.push(asteroid);
+        }
+    }
+
+    return stack;
+};
+
+
+
--- 907-Sum-Of-Subarrays-Minimum/js/main.js
+++ 907-Sum-Of-Subarrays-Minimum/js/main.js
@@ -1,1 +1,26 @@
-console.log('Hello, world! ~ JS')
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+
+
+
+
+
+var sumSubarrayMins = function(arr) {
+    let total = 0;
+    let n = arr.length;
+    for (let start = 0; start < n; start++) {
+        let subarray = [];
+        for (let end = start; end < n; end++) {
+            subarray.push(arr[end]); 
+            total += Math.min(...subarray);
+        }
+    }
+    return total;
+};
+
+
+// Example usage:
+let arr = [11,81,94,43,3];
+console.log(sumSubarrayMins(arr));
